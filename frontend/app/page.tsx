92604d4c1a7bc177c7d5496d2e08cbe87a41813e
mai <thisismainguyen@gmail.com>
2024-12-10 17:32:55 -0500

    more linting

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React from "react";
 import About from "@/components/about";
 import Project from "@/components/project";
 import Divider from "@/components/ui/divider";
